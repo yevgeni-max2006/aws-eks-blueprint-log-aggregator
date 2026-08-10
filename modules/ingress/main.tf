08b5adfa600f7cf640c56953e9587b37e2bf4e8a
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-08-11 00:25:58 +0300

    update

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -1,14 +1,14 @@
-resource "kubernetes_ingress_v1" "kong_ingress" {
-  metadata {
-    name      = "${var.name}-ingress"
-    namespace = var.namespace
 
+resource "kubernetes_ingress_v1" "grafana" {
+  metadata {
+    name      = "ingress-route-grafana"
+    namespace = "default"
     annotations = {
       "konghq.com/strip-path" = "true"
-    }
-
-    labels = {
-      app = var.name
+      # Optional:
+      # "konghq.com/protocols"                 = "https"
+      # "konghq.com/https-redirect-status-code" = "301"
+      # "cert-manager.io/cluster-issuer"      = "letsencrypt-prod"
     }
   }
 
@@ -16,27 +16,63 @@ resource "kubernetes_ingress_v1" "kong_ingress" {
     ingress_class_name = "kong"
 
     tls {
-      hosts = [
-        var.host
-      ]
-
-      secret_name = var.tls_secret_name
+      hosts       = ["logs.appflex.io"]
+      secret_name = "loki-tls"
     }
 
     rule {
-      host = var.host
-
+      host = "logs.appflex.io"
       http {
         path {
           path      = "/"
           path_type = "Prefix"
-
           backend {
             service {
-              name = var.name
-
+              name = "grafana"
               port {
-                number = var.service_port
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_ingress_v1" "minio" {
+  metadata {
+    name      = "ingress-route-minio"
+    namespace = "default"
+    annotations = {
+      "konghq.com/strip-path" = "true"
+      # Optional:
+      # "konghq.com/protocols"                  = "https"
+      # "konghq.com/https-redirect-status-code" = "301"
+      # "cert-manager.io/cluster-issuer"        = "letsencrypt-prod"
+    }
+  }
+
+  spec {
+    ingress_class_name = "kong"
+
+    tls {
+      hosts       = ["logs-s3.appflex.io"]
+      secret_name = "loki-tls"
+    }
+
+    rule {
+      host = "logs-s3.appflex.io"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "minio-console"
+              port {
+                number = 9001
               }
             }
           }
